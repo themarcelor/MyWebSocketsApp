80dd207e7ca5fbb89c74d751f9a02555899815c5
Marcelo Costa <marcelo.costa@workday.com>
2017-05-31 16:24:54 +0100

    adding license info to the class

--- src/main/java/com/themarcelor/ws/MyWebSocketsServlet.java
+++ src/main/java/com/themarcelor/ws/MyWebSocketsServlet.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* http://www.wtfpl.net/about/ */
 package com.themarcelor.ws;
 
 import java.io.IOException;
